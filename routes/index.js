13194282450f2b63e199516e665564a0a45a4d8d
Nhien Dang <danghaonhien@gmail.com>
2020-04-29 21:13:04 -0700

    fixing 3

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,10 @@ const apiRoutes = require("./apiRoutes");
 router.use("/api", apiRoutes);
 router.get("*", (req, res) => {
   if (process.env.NODE_ENV === "production") {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
   }
 });
-
+// console.log(path.join(__dirname, "build", "index.html"));
+// console.log(path.join(__dirname, "client", "build", "index.html"));
+// console.log(__dirname);
 module.exports = router;
