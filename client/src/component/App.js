794d31f729880f3e59c24ac8325ac26011feaf86
Nhien Dang <danghaonhien@gmail.com>
2020-04-29 21:48:49 -0700

    push heroku

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import Footer from "./Footer";
 import Navbar from "./NavBar";
@@ -15,16 +15,16 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/menu' component={Menu} />
-        <Route exact path='/reservation' component={Reservation} />
-        <Route exact path='/tables' component={Tables} />
-        <Route exact path='/comments' component={Comments} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/details/:customerId' component={Details} />
-        <Route exact path='/update/:customerId' component={Update} />
-      </Switch>
+
+      <Route exact path='/' component={Home} />
+      <Route exact path='/menu' component={Menu} />
+      <Route exact path='/reservation' component={Reservation} />
+      <Route exact path='/tables' component={Tables} />
+      <Route exact path='/comments' component={Comments} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/details/:customerId' component={Details} />
+      <Route exact path='/update/:customerId' component={Update} />
+
       <Footer />
     </Router>
   );
