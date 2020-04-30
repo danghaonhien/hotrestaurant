961026a34ad2eb6460b23917ecbd3db9b927cad6
Nhien Dang <danghaonhien@gmail.com>
2020-04-29 21:01:06 -0700

    fixing 3

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,16 @@ const path = require("path");
 // Hey I declared routes here.
 const apiRoutes = require("./apiRoutes");
 
-router.get("/", (req, res) => {
-  if (process.env.NODE_ENV === "production") {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  }
-});
-
 // /something was prepended to every route inside of here
 // Prepend /api to all of the routes declared inside of apiRoutes
 //  /api
 
 // This will prepend /api to anyroute declared within apiRoutes
 router.use("/api", apiRoutes);
+router.get("*", (req, res) => {
+  if (process.env.NODE_ENV === "production") {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  }
+});
 
 module.exports = router;
